fe32075489efe65af9501c50370b8d87fa8782ac
Heidi Erwin <heidianneerwin@gmail.com>
2018-07-17 16:44:13 -0700

    added new scene w/ three fourths view and player movement using arrow keys

---
+++ LanguageProjectUnity/Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * NPC and Player will both extend Character (those will likely be the only two types of Characters)
+ */
+public abstract class Character : MonoBehaviour {
+
+    public const int NORTH = 0;
+    public const int EAST = 1;
+    public const int SOUTH = 2;
+    public const int WEST = 3;
+
+    [SerializeField]
+    protected float speed;
+    protected Vector2 velocity;
+    protected int directionFacing;
+
+    protected virtual void Update() {
+        Move();
+    }
+
+    /**
+     * Updates the transform (position) of this Character based on the velocity field of
+     * the character. 
+     */
+    public void Move() {
+        transform.Translate(velocity * speed * Time.deltaTime);
+    }
+
+}
---
+++ LanguageProjectUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+    protected override void Update() {
+        GetInput();
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        base.Update();
+    }
+
+    /**
+    * Moves the Player based on arrow key input. Or rather, sets the
+    * velocity field of the player so that Move() can update the player's 
+    * location properly.
+    */
+    private void GetInput() {
+        velocity = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            directionFacing = NORTH;
+            velocity += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            directionFacing = EAST;
+            velocity += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            directionFacing = SOUTH;
+            velocity += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            directionFacing = WEST;
+            velocity += Vector2.left;
+        }
+    }
+}
