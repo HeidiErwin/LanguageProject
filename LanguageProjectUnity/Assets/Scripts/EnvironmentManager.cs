712b916f5b8ec54b3f411000137a358b2e642e19
Heidi Erwin <heidianneerwin@gmail.com>
2018-09-19 10:51:14 -0400

    created file for EnvironmentManager; game object in scene

---
+++ LanguageProjectUnity/Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class sends perception messages to NPCs, communicating 
+// between objects of the world and the NPCs perceiving them.
+public class EnvironmentManager : MonoBehaviour {
+
+
+}
